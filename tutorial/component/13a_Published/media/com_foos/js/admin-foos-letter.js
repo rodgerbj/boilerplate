b6a6c43de05c867fd4ec79528da48636b3e9cbda
astridx <info@astrid-guenther.de>
2019-07-01 13:37:20 +0200

    wip

--- tutorial/component/13a_Published/media/com_foos/js/admin-foos-letter.js
+++ tutorial/component/13a_Published/media/com_foos/js/admin-foos-letter.js
@@ -1,29 +1,17 @@
-if (document.formvalidator) {
-	document.formvalidator.setHandler('letter', function (value) {
-
-		var returnedValue = false;
-
-		regex = /^([a-z]+)$/i;
-
-		if (regex.test(value))
-			returnedValue = true;
-
-		return returnedValue;
-	});
-} else {
-	setTimeout(function ()
-	{
+document.addEventListener('DOMContentLoaded', function(){
+	"use strict";
+	if (document.formvalidator) {
 		document.formvalidator.setHandler('letter', function (value) {
 
 			var returnedValue = false;
 
-			regex = /^([a-z]+)$/i;
-			
+			var regex = /^([a-z]+)$/i;
+
 			if (regex.test(value))
 				returnedValue = true;
 
 			return returnedValue;
 		});
-
-	}, 1000);
-}
+		//console.log(document.formvalidator);
+	} 
+});
