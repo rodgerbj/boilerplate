0f9c72e33d9f7c6b3e21b69c795d98685cd25f74
astridx <info@astrid-guenther.de>
2019-08-12 14:28:44 +0200

    wip

--- dist/foos-0.0.1/components/com_foos/View/Foo/HtmlView.php
+++ dist/foos-0.0.1/components/com_foos/View/Foo/HtmlView.php
@@ -1,33 +1,31 @@
 <?php
 /**
- * @package     Joomla.Administrator
+ * @package     Joomla.Site
  * @subpackage  com_foos
  *
  * @copyright   Copyright (C) 2005 - 2019 Open Source Matters, Inc. All rights reserved.
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
  */
 
-namespace Joomla\Component\Foos\Administrator\View\Foos;
+namespace Joomla\Component\Foos\Site\View\Foo;
 
 defined('_JEXEC') or die;
 
 use Joomla\CMS\MVC\View\HtmlView as BaseHtmlView;
 
 /**
- * View class for a list of foos.
+ * HTML Foos View class for the Foo component
  *
  * @since  1.0
  */
 class HtmlView extends BaseHtmlView
 {
 	/**
-	 * Method to display the view.
+	 * Execute and display a template script.
 	 *
-	 * @param   string  $tpl  A template file to load. [optional]
+	 * @param   string  $tpl  The name of the template file to parse; automatically searches through the template paths.
 	 *
-	 * @return  mixed  A string if successful, otherwise an \Exception object.
-	 *
-	 * @since   1.0
+	 * @return  mixed  A string if successful, otherwise an Error object.
 	 */
 	public function display($tpl = null)
 	{
