762d0310d581477128add6bc5b575e2b926d357c
astridx <info@astrid-guenther.de>
2019-08-15 12:07:16 +0200

    wip

--- src/administrator/components/com_foos/Model/FoosModel.php
+++ src/administrator/components/com_foos/Model/FoosModel.php
@@ -47,7 +47,16 @@ class FoosModel extends ListModel
 
 		// Select the required fields from the table.
 		$query->select(
-			$db->quoteName(array('a.id', 'a.name', 'a.catid', 'a.access'))
+			$this->getState(
+				'list.select',
+				'a.id AS id,'
+				. 'a.name AS name,'
+				. 'a.state AS state,'
+				. 'a.catid AS catid,'
+				. 'a.published AS published,'
+				. 'a.publish_up,'
+				. 'a.publish_down'
+			)
 		);
 
 		$query->from($db->quoteName('#__foos_details', 'a'));
