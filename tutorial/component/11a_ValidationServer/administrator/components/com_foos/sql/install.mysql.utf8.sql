6188d867adc819588985b2c7691a3f64fef1f982
astridx <info@astrid-guenther.de>
2019-08-18 20:01:16 +0200

    wip#

--- tutorial/component/11a_ValidationServer/administrator/components/com_foos/sql/install.mysql.utf8.sql
+++ tutorial/component/11a_ValidationServer/administrator/components/com_foos/sql/install.mysql.utf8.sql
@@ -1,10 +1,8 @@
 CREATE TABLE IF NOT EXISTS `#__foos_details` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `alias` varchar(400) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL DEFAULT '',
-  `access` int(10) unsigned NOT NULL DEFAULT 0,
   `name` varchar(255) NOT NULL DEFAULT '',
-  PRIMARY KEY (`id`),
-  KEY `idx_access` (`access`)
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 DEFAULT COLLATE=utf8mb4_unicode_ci;
 
 INSERT INTO `#__foos_details` (`name`) VALUES
