20df401d9d09c7b57210eadda2bc6b54020339e7
astridx <info@astrid-guenther.de>
2019-08-18 17:54:06 +0200

    v9

--- src/media/com_foos/js/admin-foos-modal.js
+++ src/media/com_foos/js/admin-foos-modal.js
@@ -1,3 +1,12 @@
+/**
+* PLEASE DO NOT MODIFY THIS FILE. WORK ON THE ES6 VERSION.
+* OTHERWISE YOUR CHANGES WILL BE REPLACED ON THE NEXT BUILD.
+**/
+
+/**
+ * @copyright  Copyright (C) 2005 - 2019 Open Source Matters, Inc. All rights reserved.
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ */
 (function () {
   'use strict';
   /**
