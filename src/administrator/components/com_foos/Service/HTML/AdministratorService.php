5573def429086b33ae5b7bc0fb69259e5994fde0
astridx <info@astrid-guenther.de>
2019-08-20 21:55:14 +0200

    initial

---
+++ src/administrator/components/com_foos/Service/HTML/AdministratorService.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * @package     Joomla.Administrator
+ * @subpackage  com_foos
+ *
+ * @copyright   Copyright (C) 2005 - 2019 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+namespace Joomla\Component\Foos\Administrator\Service\HTML;
+
+defined('JPATH_BASE') or die;
+
+/**
+ * Foo HTML class.
+ *
+ * @since  1.0
+ */
+class AdministratorService
+{
+}
