d441712bf28df58ac4761aee55c88f55346454a7
astridx <info@astrid-guenther.de>
2019-10-17 22:56:00 +0200

    wip#

--- src/components/com_foos/tmpl/foo/default.php
+++ src/components/com_foos/tmpl/foo/default.php
@@ -10,8 +10,4 @@ defined('_JEXEC') or die;
 
 use Joomla\CMS\Language\Text;
 
-if ($this->get('State')->get('params')->get('show_foo_name_label')) {
-	echo Text::_('COM_FOOS_NAME') . $this->item->name;
-} else {
-	echo $this->item->name;
-}
+echo Text::_('COM_FOOS_NAME') . $this->item->name;
