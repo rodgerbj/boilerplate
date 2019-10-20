61f07765611a3780ce11c9c0581ea7a2147237fb
astridx <info@astrid-guenther.de>
2019-10-20 13:27:31 +0200

    wip

--- src/administrator/components/com_foos/sql/install.mysql.utf8.sql
+++ src/administrator/components/com_foos/sql/install.mysql.utf8.sql
@@ -27,3 +27,7 @@ ALTER TABLE `#__foos_details` ADD COLUMN  `publish_up` datetime AFTER `alias`;
 ALTER TABLE `#__foos_details` ADD COLUMN  `publish_down` datetime AFTER `alias`;
 
 ALTER TABLE `#__foos_details` ADD KEY `idx_state` (`published`);
+
+ALTER TABLE `#__foos_details` ADD COLUMN  `language` char(7) NOT NULL DEFAULT '*' AFTER `alias`;
+
+ALTER TABLE `#__foos_details` ADD KEY `idx_language` (`language`);
