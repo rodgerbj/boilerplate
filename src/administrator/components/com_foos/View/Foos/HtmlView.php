e98a6e880c6e8c48f19cb1f8547b45ea9b81e661
astridx <info@astrid-guenther.de>
2020-01-29 11:14:53 +0100

    https://github.com/joomla/joomla-cms/pull/27678

--- src/administrator/components/com_foos/View/Foos/HtmlView.php
+++ src/administrator/components/com_foos/View/Foos/HtmlView.php
@@ -148,23 +148,4 @@ class HtmlView extends BaseHtmlView
 
 		HTMLHelper::_('sidebar.setAction', 'index.php?option=com_foos');
 	}
-	/**
-	 * Returns an array of fields the table can be sorted by
-	 *
-	 * @return  array  Array containing the field name to sort by as the key and display text as value
-	 *
-	 * @since   1.0
-	 */
-	protected function getSortFields()
-	{
-		return array(
-			'a.ordering'     => Text::_('JGRID_HEADING_ORDERING'),
-			'a.published'    => Text::_('JSTATUS'),
-			'a.name'         => Text::_('JGLOBAL_TITLE'),
-			'category_title' => Text::_('JCATEGORY'),
-			'a.access'       => Text::_('JGRID_HEADING_ACCESS'),
-			'a.language'     => Text::_('JGRID_HEADING_LANGUAGE'),
-			'a.id'           => Text::_('JGRID_HEADING_ID'),
-		);
-	}
 }
