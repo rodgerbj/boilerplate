0916a7dc707616f1aa11c462bf2e4ab9a54267f7
astridx <info@astrid-guenther.de>
2020-02-19 22:17:31 +0100

    wip

--- src/components/com_foos/src/View/Foo/HtmlView.php
+++ src/components/com_foos/src/View/Foo/HtmlView.php
@@ -12,8 +12,6 @@ namespace Joomla\Component\Foos\Site\View\Foo;
 defined('_JEXEC') or die;
 
 use Joomla\CMS\MVC\View\HtmlView as BaseHtmlView;
-use Joomla\CMS\Factory;
-use Joomla\Registry\Registry;
 
 /**
  * HTML Foos View class for the Foo component
@@ -22,22 +20,6 @@ use Joomla\Registry\Registry;
  */
 class HtmlView extends BaseHtmlView
 {
-	/**
-	 * The page parameters
-	 *
-	 * @var    \Joomla\Registry\Registry|null
-	 * @since  1.0.0
-	 */
-	protected $params = null;
-
-	/**
-	 * The item model state
-	 *
-	 * @var    \Joomla\Registry\Registry
-	 * @since  1.0.0
-	 */
-	protected $state;
-
 	/**
 	 * The item object details
 	 *
@@ -55,27 +37,7 @@ class HtmlView extends BaseHtmlView
 	 */
 	public function display($tpl = null)
 	{
-		$item = $this->item = $this->get('Item');
-		$state = $this->State = $this->get('State');
-		$params = $this->Params = $state->get('params');
-		$itemparams = new Registry(json_decode($item->params));
-
-		$temp = clone $params;
-		$temp->merge($itemparams);
-		$item->params = $temp;
-
-		Factory::getApplication()->triggerEvent('onContentPrepare', array ('com_foos.foo', &$item));
-
-		// Store the events for later
-		$item->event = new \stdClass;
-		$results = Factory::getApplication()->triggerEvent('onContentAfterTitle', array('com_foos.foo', &$item, &$item->params));
-		$item->event->afterDisplayTitle = trim(implode("\n", $results));
-
-		$results = Factory::getApplication()->triggerEvent('onContentBeforeDisplay', array('com_foos.foo', &$item, &$item->params));
-		$item->event->beforeDisplayContent = trim(implode("\n", $results));
-
-		$results = Factory::getApplication()->triggerEvent('onContentAfterDisplay', array('com_foos.foo', &$item, &$item->params));
-		$item->event->afterDisplayContent = trim(implode("\n", $results));
+		$this->item = $this->get('Item');
 
 		return parent::display($tpl);
 	}
