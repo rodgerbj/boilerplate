cd2472a466b4fc9ce080fa1f18500b81981566bf
astridx <info@astrid-guenther.de>
2020-03-20 09:48:15 +0100

    wip

--- src/components/com_foos/src/Service/Category.php
+++ src/components/com_foos/src/Service/Category.php
@@ -9,7 +9,7 @@
 
 namespace Joomla\Component\Foos\Site\Service;
 
-defined('_JEXEC') or die;
+\defined('_JEXEC') or die;
 
 use Joomla\CMS\Categories\Categories;
 
--- src/components/com_foos/src/View/Category/HtmlView.php
+++ src/components/com_foos/src/View/Category/HtmlView.php
@@ -9,7 +9,7 @@
 
 namespace Joomla\Component\Foos\Site\View\Category;
 
-defined('_JEXEC') or die;
+\defined('_JEXEC') or die;
 
 use Joomla\CMS\MVC\View\CategoryView;
 use Joomla\Component\Foos\Site\Helper\RouteHelper;
