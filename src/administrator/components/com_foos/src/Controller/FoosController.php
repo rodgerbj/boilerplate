6c1cb6cb81008d9a65f9dab48188920eb83ce16b
astridx <info@astrid-guenther.de>
2020-09-17 15:06:39 +0200

    namespace

--- src/administrator/components/com_foos/src/Controller/FoosController.php
+++ src/administrator/components/com_foos/src/Controller/FoosController.php
@@ -7,7 +7,7 @@
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
  */
 
-namespace Joomla\Component\Foos\Administrator\Controller;
+namespace FooNamespace\Component\Foos\Administrator\Controller;
 
 \defined('_JEXEC') or die;
 
