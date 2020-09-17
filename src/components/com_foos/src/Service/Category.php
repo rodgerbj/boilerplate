b187fecb2d9271603f83f8d6f317087735bd1076
astridx <info@astrid-guenther.de>
2020-09-17 15:06:49 +0200

    namespace

--- src/components/com_foos/src/Service/Category.php
+++ src/components/com_foos/src/Service/Category.php
@@ -7,7 +7,7 @@
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
  */
 
-namespace Joomla\Component\Foos\Site\Service;
+namespace FooNamespace\Component\Foos\Site\Service;
 
 \defined('_JEXEC') or die;
 
