baa947b9d38b51bb8f5acb5a10f4ba17c780d320
astridx <info@astrid-guenther.de>
2021-03-07 10:20:23 +0100

    cs

--- src/components/com_foos/tmpl/featured/default_items.php
+++ src/components/com_foos/tmpl/featured/default_items.php
@@ -26,7 +26,6 @@ $params = &$this->item->params;
 	<?php if (empty($this->items)) : ?>
 		<p class="com-foos-featured__message"> <?php echo Text::_('COM_FOO_NO_FOOS'); ?>	 </p>
 	<?php else : ?>
-
 	<form action="<?php echo htmlspecialchars(Uri::getInstance()->toString()); ?>" method="post" name="adminForm" id="adminForm">
 		<table class="com-foos-featured__table table">
 			<?php if ($this->params->get('show_headings')) : ?>
