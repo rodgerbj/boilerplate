bc5fbbc5e54066a4073d7093ae21a6742e4d42a5
astridx <info@astrid-guenther.de>
2021-03-26 13:32:38 +0100

    cs

--- src/templates/facile/index.php
+++ src/templates/facile/index.php
@@ -11,69 +11,71 @@
 
 <!DOCTYPE html>
 <html lang="de">
-  <head>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Titel</title>
-  </head>
 
-  <body>
-  <header>
-	<div>
-	  <nav>
-				<div>
-					<jdoc:include type="modules" name="menu" />
-				</div>
-			</nav>
-			<div>
-				<jdoc:include type="modules" name="search" />
-			</div>
-		</div>
-	</header>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Titel</title>
+</head>
 
-	<div>
-		<jdoc:include type="modules" name="banner" />
-	</div>
+<body>
+    <header>
+        <div>
+            <nav>
+                <div>
+                    <jdoc:include type="modules" name="menu" />
+                </div>
+            </nav>
+            <div>
+                <jdoc:include type="modules" name="search" />
+            </div>
+        </div>
+    </header>
 
-	<div>
-		<jdoc:include type="modules" name="top-a" />
-	</div>
+    <div>
+        <jdoc:include type="modules" name="banner" />
+    </div>
 
-	<div>
-		<jdoc:include type="modules" name="top-b" />
-	</div>
+    <div>
+        <jdoc:include type="modules" name="top-a" />
+    </div>
 
-	<div>
-		<jdoc:include type="modules" name="sidebar-left" />
-	</div>
+    <div>
+        <jdoc:include type="modules" name="top-b" />
+    </div>
 
-	<div>
-		<jdoc:include type="modules" name="breadcrumbs" />
-		<jdoc:include type="modules" name="main-top" />
-		<jdoc:include type="message" />
-		<main>
-		<jdoc:include type="component" />
-		</main>
-		<jdoc:include type="modules" name="main-bottom" />
-	</div>
+    <div>
+        <jdoc:include type="modules" name="sidebar-left" />
+    </div>
 
-	<div>
-		<jdoc:include type="modules" name="sidebar-right" />
-	</div>
+    <div>
+        <jdoc:include type="modules" name="breadcrumbs" />
+        <jdoc:include type="modules" name="main-top" />
+        <jdoc:include type="message" />
+        <main>
+            <jdoc:include type="component" />
+        </main>
+        <jdoc:include type="modules" name="main-bottom" />
+    </div>
 
-	<div>
-		<jdoc:include type="modules" name="bottom-a" />
-	</div>
+    <div>
+        <jdoc:include type="modules" name="sidebar-right" />
+    </div>
 
-	<div>
-		<jdoc:include type="modules" name="bottom-b" />
-	</div>
+    <div>
+        <jdoc:include type="modules" name="bottom-a" />
+    </div>
 
-	<footer>
-		<jdoc:include type="modules" name="footer" />
-	</footer>
+    <div>
+        <jdoc:include type="modules" name="bottom-b" />
+    </div>
 
-  <jdoc:include type="modules" name="debug" />
+    <footer>
+        <jdoc:include type="modules" name="footer" />
+    </footer>
+
+    <jdoc:include type="modules" name="debug" />
 
 </body>
+
 </html>
