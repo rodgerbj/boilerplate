65d2b0ad51e583efb9311b0af35eef5e9cfebe4b
astridx <info@astrid-guenther.de>
2021-04-12 14:18:22 +0200

    wip

--- duplicate.sh
+++ duplicate.sh
@@ -7,7 +7,6 @@ EX="AGADVENT"
 mkdir "new_folder"
 
 
-
 cp changelog.xml ./new_folder/
 cp composer.json ./new_folder/
 cp foo_update.xml ./new_folder/
@@ -19,11 +18,35 @@ cp README.md ./new_folder/
 cp RoboFile.php ./new_folder/
 
 
-#Rename Files
-find ./new_folder -type f -name '*foo*' | while read FILE ; do
-    newfile="$(echo ${FILE} |sed -e 's/foo/agadvent/')" ;
-    mv "${FILE}" "${newfile}" ;
-done 
-#find ./new_folder -name '*foo*' -exec bash -c ' mv $0 ${0/foo/agadvent}' {} \;
+# Footer rename for sik
+find ./new_folder -type d -print0 | xargs -0 rename s/Footer/Fzzter/g
+find ./new_folder -type f -print0 | xargs -0 rename s/Footer/Fzzter/g
+find ./new_folder \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i s/Footer/Fzzter/g
 
-#Rename in File
+#Rename Files and Folders
+find ./new_folder -type d -print0 | xargs -0 rename s/foo/$ex/g
+find ./new_folder -type f -print0 | xargs -0 rename s/foo/$ex/g
+find ./new_folder -type d -print0 | xargs -0 rename s/Foo/$Ex/g
+find ./new_folder -type f -print0 | xargs -0 rename s/Foo/$Ex/g
+# hack?
+find ./new_folder -type d -print0 | xargs -0 rename s/foo/$ex/g
+find ./new_folder -type f -print0 | xargs -0 rename s/foo/$ex/g
+find ./new_folder -type d -print0 | xargs -0 rename s/Foo/$Ex/g
+find ./new_folder -type f -print0 | xargs -0 rename s/Foo/$Ex/g
+
+#Rename in Files https://stackoverflow.com/questions/1583219/how-to-do-a-recursive-find-replace-of-a-string-with-awk-or-sed
+find ./new_folder \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i s/foo/$ex/g
+find ./new_folder \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i s/Foo/$Ex/g
+find ./new_folder \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i s/FOO/$EX/g
+
+# Footer rename again
+find ./new_folder -type d -print0 | xargs -0 rename s/Fzzter/Footer/g
+find ./new_folder -type f -print0 | xargs -0 rename s/Fzzter/Footer/g
+find ./new_folder \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i s/Fzzter/Footer/g
+
+
+#Build zip docker-lamp server need to be active
+docker exec -it --user 1000 -w /home/astrid/git/joomla-development/btutorial/t43_umbennen/new_folder docker-lamp_php73 composer install
+docker exec -it --user 1000 -w /home/astrid/git/joomla-development/btutorial/t43_umbennen/new_folder docker-lamp_php73 ./vendor/bin/robo build
+
+
