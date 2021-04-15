f10734632ae7310fba89aebe4ad072b6e2e91b51
astridx <info@astrid-guenther.de>
2021-04-15 15:05:23 +0200

    cs

--- src/templates/facile/index.php
+++ src/templates/facile/index.php
@@ -123,7 +123,7 @@ $wa->registerAndUseScript('main', $templatePath . '/assets/js/main.js', [], ['de
             </div>
         </section>
 
-        <agadventter id="agadventter">
+        < id="">
             <?php if ($this->params->get('showFooter')) : ?>
             <div class="col-12">
                 <section>
@@ -153,12 +153,12 @@ $wa->registerAndUseScript('main', $templatePath . '/assets/js/main.js', [], ['de
             <?php endif; ?>
 
 
-            <?php if ($this->countModules('agadventter', true)) : ?>
+            <?php if ($this->countModules('', true)) : ?>
             <div id="copyright">
-                <jdoc:include type="modules" name="agadventter" />
+                <jdoc:include type="modules" name="" />
             </div>
             <?php endif; ?>
-        </agadventter>
+        </>
 
         <jdoc:include type="modules" name="debug" />
     </div>
