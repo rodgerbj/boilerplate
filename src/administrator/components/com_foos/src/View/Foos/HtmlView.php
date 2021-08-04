feeb1458f95953a75d42201ba46bb30d1f2ff167
astridx <info@astrid-guenther.de>
2021-08-04 23:01:15 +0200

    cs

--- src/administrator/components/com_foos/src/View/Foos/HtmlView.php
+++ src/administrator/components/com_foos/src/View/Foos/HtmlView.php
@@ -16,6 +16,7 @@ use Joomla\CMS\Language\Text;
 use Joomla\CMS\MVC\View\HtmlView as BaseHtmlView;
 use Joomla\CMS\Toolbar\Toolbar;
 use Joomla\CMS\Toolbar\ToolbarHelper;
+use Joomla\CMS\Factory;
 
 /**
  * View class for a list of foos.
